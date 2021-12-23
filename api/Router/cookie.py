ce58814f6a82025064bdaa78f63ff4d7fe532810
someh2705 <someh2705@gmail.com>
2021-12-24 04:10:48 +0900

    feat: GET /cookie_id/{cookie_id}/info api 추가

--- api/Router/cookie.py
+++ api/Router/cookie.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from Cookie.core.Items import items
 
 router = APIRouter(
     prefix="/cookie/v1"
 )
 
 @router.get('/cookie_id/{cookie_id}/info')
-async def read_cookie(cookie_id: int):
-    return {'cookie_id': cookie_id}
+async def read_cookie_info(cookie_id: int):
+    for item in items:
+        if item.cookie_id == cookie_id:
+            return item.asJsonInfo()
+    raise HTTPException(status_code=404, detail="Not Found Error")
+
+@router.get('/cookie_id/{cookie_id}/detail')
+async def read_cookie_detail(cookie_id: int):
+    return {'cookie_id': cookie_id, 'detail': None}
