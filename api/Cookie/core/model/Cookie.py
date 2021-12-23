baeadba0057bdcb5ec5f28a20d399db32e8090dc
someh2705 <someh2705@gmail.com>
2021-12-24 04:09:05 +0900

    feat: 쿠키 데이터 클래스 추가 및 json파싱 함수 추가

--- api/Cookie/core/model/Cookie.py
+++ api/Cookie/core/model/Cookie.py
@@ -1,13 +1,36 @@
 from fastapi.responses import FileResponse
-
-from api.main import Gender
+from Cookie.core.model.Gender import CookieGender
+from Cookie.core.model.Grade import CookieGrade
+from Cookie.core.model.Skill import CookieSkill
+from Cookie.core.model.Type import CookieType
 
 class Cookie:
     def __init__(
         self,
+        *,
         cookie_id: int,
         cookie_name: str,
         cookie_image: FileResponse,
-        gender: Gender
+        cookie_gender: CookieGender,
+        cookie_type: CookieType,
+        cookie_grade: CookieGrade,
+        cookie_skill: CookieSkill
     ) -> None:
-        pass
+        self.cookie_id = cookie_id
+        self.cookie_name = cookie_name
+        self.cookie_image = cookie_image
+        self.cookie_gender = cookie_gender
+        self.cookie_type = cookie_type
+        self.cookie_grade = cookie_grade
+        self.cookie_skill = cookie_skill
+
+    def asJsonInfo(self):
+        return {
+            "cookie_id": self.cookie_id,
+            "cookie_name": self.cookie_name,
+            "cookie_image": self.cookie_image,
+            "cookie_gender": self.cookie_gender,
+            "cookie_type": self.cookie_type,
+            "cookie_grade": self.cookie_grade,
+            "cookie_skill": self.cookie_skill,
+        }
