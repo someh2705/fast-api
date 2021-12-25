85fb3a17544cc0b72310e316eaaa1d749dab919d
someh2705 <someh2705@gmail.com>
2021-12-25 23:57:10 +0900

    feat: 쿠키 데이터 불러오기 api 추가

--- api/Router/cookie.py
+++ api/Router/cookie.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter, HTTPException
 from Cookie.core.Items import items
+from Cookie.core.model.Cookie import Cookie
 
 router = APIRouter(
     prefix="/cookie/v1"
 )
 
-@router.get('/cookie_id/{cookie_id}/info')
-async def read_cookie_info(cookie_id: int):
+@router.get('/cookie_id/list')
+async def read_cookie_list(start: int, length: int):
+    if start + length >= len(items):
+        raise HTTPException(status_code=404, detail="Out of Index")
+
+    return list(map(Cookie.asJsonInfo, items[start:start+length+1]))
+
+@router.get('/cookie_id/detail')
+async def read_cookie_detail(cookie_id: int):
     for item in items:
         if item.cookie_id == cookie_id:
-            return item.asJsonInfo()
+            return item.asJsonDetail()
     raise HTTPException(status_code=404, detail="Not Found Error")
-
-@router.get('/cookie_id/{cookie_id}/detail')
-async def read_cookie_detail(cookie_id: int):
-    return {'cookie_id': cookie_id, 'detail': None}
