e29c9f10133687c41a8e08201a683180b3973ca8
someh2705 <someh2705@gmail.com>
2021-12-25 23:44:57 +0900

    feat: 쿠키 포지션 추가

--- api/Cookie/core/model/Cookie.py
+++ api/Cookie/core/model/Cookie.py
@@ -1,6 +1,7 @@
 from fastapi.responses import FileResponse
 from Cookie.core.model.Gender import CookieGender
 from Cookie.core.model.Grade import CookieGrade
+from Cookie.core.model.Position import CookiePosition
 from Cookie.core.model.Skill import CookieSkill
 from Cookie.core.model.Type import CookieType
 
@@ -9,28 +10,38 @@ class Cookie:
         self,
         *,
         cookie_id: int,
-        cookie_name: str,
         cookie_image: FileResponse,
+        cookie_name: str,
+        cookie_grade: CookieGrade,
         cookie_gender: CookieGender,
         cookie_type: CookieType,
-        cookie_grade: CookieGrade,
+        cookie_position = CookiePosition,
         cookie_skill: CookieSkill
     ) -> None:
         self.cookie_id = cookie_id
-        self.cookie_name = cookie_name
         self.cookie_image = cookie_image
+        self.cookie_name = cookie_name
+        self.cookie_grade = cookie_grade
         self.cookie_gender = cookie_gender
         self.cookie_type = cookie_type
-        self.cookie_grade = cookie_grade
+        self.cookie_position = cookie_position
         self.cookie_skill = cookie_skill
 
     def asJsonInfo(self):
         return {
             "cookie_id": self.cookie_id,
             "cookie_name": self.cookie_name,
+            "cookie_grade": self.cookie_grade,
+        }
+
+    def asJsonDetail(self):
+        return {
+            "cookie_id": self.cookie_id,
             "cookie_image": self.cookie_image,
+            "cookie_name": self.cookie_name,
+            "cookie_grade": self.cookie_grade,
             "cookie_gender": self.cookie_gender,
             "cookie_type": self.cookie_type,
-            "cookie_grade": self.cookie_grade,
+            "cookie_position": self.cookie_position,
             "cookie_skill": self.cookie_skill,
         }
---
+++ api/Cookie/core/model/Position.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class CookiePosition(Enum):
+    FRONT = "전방",
+    MIDDLE = "중앙",
+    BACK = "후방",
